6597bab7260a20ac494ab37b60842a5530bde21c
Harry Yu <hy.harry.yu@gmail.com>
2019-08-02 19:07:53 -0700

    Disabled minification

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,4 +3,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false,
+  },
 });
